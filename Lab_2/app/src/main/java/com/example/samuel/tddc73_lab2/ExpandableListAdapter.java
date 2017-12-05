8d5605eea19588eb19fc3e63be5d38661008553a
Samuel <samuel0808@gmail.com>
2017-12-05 18:29:47 +0100

    Adapter working, search not implemented.

--- Lab_2/app/src/main/java/com/example/samuel/tddc73_lab2/ExpandableListAdapter.java
+++ Lab_2/app/src/main/java/com/example/samuel/tddc73_lab2/ExpandableListAdapter.java
@@ -3,6 +3,7 @@ package com.example.samuel.tddc73_lab2;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,99 +25,109 @@ import java.util.Map;
 
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private final Context context;
-    private final HashMap<String, ArrayList<String>> parentList;
-    private final HashMap<String, ArrayList<String>> originalParentList;
+    private final ArrayList<Pair<String, ArrayList<String>>> parentList;
+    private final ArrayList<Pair<String, ArrayList<String>>> originalParentList;
 
-    public ExpandableListAdapter(Context context, HashMap<String, ArrayList<String>> parentList) {
-        //System.out.println("constructor" + parentList);
+    public ExpandableListAdapter(Context context, ArrayList<Pair<String, ArrayList<String>>> parentList) {
+        System.out.println("constructor" + parentList);
         this.context = context;
-        this.parentList = new HashMap<>();
-        this.parentList.putAll(parentList);
-        this.originalParentList = new HashMap<>();
-        this.originalParentList.putAll(parentList);
-        searchData("a");
+        this.parentList = new ArrayList<>();
+        this.parentList.addAll(parentList);
+        this.originalParentList = new ArrayList<>();
+        this.originalParentList.addAll(parentList);
+        //searchData("falafel");
 
     }
 
     @Override
     public int getGroupCount() {
+        Log.d("", "getGroupCount: return " + parentList.size());
         return parentList.size();
     }
 
     @Override
     public int getChildrenCount(int parentPosition) {
-        return parentList.get(parentPosition).size();
+        Log.d("", "getChildrenCount:" + parentPosition);
+        return parentList.get(parentPosition).second.size();
     }
 
     @Override
-    public Object getGroup(int parentPosition) {
-        System.out.println("getGroup, parentPosition " + parentPosition);
-        System.out.println("parentList "+ parentList);
-        return parentList.get(parentPosition);
+    public String getGroup(int parentPosition) {
+        Log.d("", "getGroup:" + parentPosition);
+        return parentList.get(parentPosition).first;
     }
 
     @Override
-    public Object getChild(int i, int i1) {
-        return null;
+    public String getChild(int parentPosition, int childPosition) {
+        Log.d("", "getChild:" + parentPosition + childPosition);
+        return parentList.get(parentPosition).second.get(childPosition);
     }
-
     @Override
     public long getGroupId(int parentPosition) {
+        Log.d("", "getGroupId:" + parentPosition);
         return parentPosition;
     }
 
     @Override
     public long getChildId(int parentPosition , int childPosition) {
+        Log.d("", "getChildId:" + parentPosition);
         return childPosition;
     }
 
     @Override
     public boolean hasStableIds() {
+        Log.d("", "hasStableIds: ");
         return false;
     }
 
     @Override
     public View getGroupView(int parentPosition, boolean isLastChild, View view, ViewGroup parent) {
-        System.out.println("GET group view " + parentPosition);
-        ArrayList childList = (ArrayList) getGroup(parentPosition);
+        Log.d("", "getGroupView: " + parentPosition);
+
         if( view == null){
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = layoutInflater.inflate(R.layout.activity_main, null);
+            view = layoutInflater.inflate(R.layout.child_items, null);
         }
 
-        TextView heading = (TextView) view.findViewById(R.id.heading);
-        heading.setText("Papa poop");
+        TextView heading = (TextView) view;
+        if (heading == null){
+            System.out.println("its NULL!");
+        }
+        heading.setText(parentList.get(parentPosition).first);
         return view;
     }
 
     @Override
     public View getChildView(int parentPosition, int childPosition, boolean isLastChild, View view, ViewGroup parent) {
-        String child = (String) getChild(parentPosition, childPosition);
-        System.out.println("GET child view "+ child);
+        Log.d("", "getChildView: " + parentPosition + childPosition);
+        String child = getChild(parentPosition, childPosition);
+
         if(view == null){
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = layoutInflater.inflate(R.layout.activity_main, null);
+            view = layoutInflater.inflate(R.layout.child_items, null);
         }
-        TextView heading = (TextView) view.findViewById(R.id.child);
-        heading.setText("Child poop");
+        TextView heading = (TextView) view;
+        heading.setText(child);
 
         return view;
     }
 
     @Override
     public boolean isChildSelectable(int i, int i1) {
+        Log.d("", "isChildSelectable: ");
         return true;
     }
 
     public void searchData(String query){
+        Log.d("", "searchData: " + query);
         query = query.toLowerCase();
         parentList.clear();
 
         if(query.isEmpty()){
             System.out.println("query is empty");
-            parentList.putAll(originalParentList);
+            parentList.addAll(originalParentList);
         }
-        else {
+        /*else {
             Iterator<Map.Entry<String, ArrayList<String>>> iterator = originalParentList.entrySet().iterator();
             System.out.println();
             while (iterator.hasNext()) {
@@ -131,11 +142,11 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                 }
                 if (newList.size() > 0) {
                     System.out.println("SIZE" + newList.size());
-                    parentList.put(currentValue.getKey(), newList);
+                    parentList.add(currentValue.getKey(), newList);
                     iterator.remove(); // avoids a ConcurrentModificationException
                 }
             }
-        }
+        }*/
         System.out.println("parent list after search" + parentList);
         notifyDataSetChanged();
     }
--- Lab_2/app/src/main/java/com/example/samuel/tddc73_lab2/MainActivity.java
+++ Lab_2/app/src/main/java/com/example/samuel/tddc73_lab2/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.ArrayMap;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -15,11 +16,13 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements ExpandableListView.OnChildClickListener{
 
-    private HashMap<String, ArrayList<String>> data = new HashMap<>();
+
+    private ArrayList<Pair<String, ArrayList<String>>> newData = new ArrayList<>();
     private com.example.samuel.tddc73_lab2.ExpandableListAdapter expandableListAdapter;
 
     @Override
@@ -37,25 +40,27 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
     }
 
     public void populateData(){
-        ArrayList<String> food = new ArrayList<>();
-        food.add("Humus");
-        food.add("Falafel");
 
         ArrayList<String> vehicles = new ArrayList<>();
         vehicles.add("Dragon");
         vehicles.add("Falafel");
 
+        ArrayList<String> food = new ArrayList<>();
+        food.add("Humus");
+        food.add("Falafel");
 
+        Pair<String, ArrayList<String>> vehiclePair = new Pair<>("Vehicles", vehicles);
+        Pair<String, ArrayList<String>> foodPair = new Pair<>("Food", food);
+        newData.add(vehiclePair);
+        newData.add(foodPair);
 
-        data.put("Food", food);
-        data.put("Vehicles", vehicles);
 
     }
     private void displayList() {
         //get reference to the ExpandableListView
         ExpandableListView listView = findViewById(R.id.ListView);
         //create the adapter by passing ArrayList data
-        expandableListAdapter = new com.example.samuel.tddc73_lab2.ExpandableListAdapter(MainActivity.this, data);
+        expandableListAdapter = new com.example.samuel.tddc73_lab2.ExpandableListAdapter(MainActivity.this, newData);
         //attach the adapter to the list
         listView.setAdapter(expandableListAdapter);
 
