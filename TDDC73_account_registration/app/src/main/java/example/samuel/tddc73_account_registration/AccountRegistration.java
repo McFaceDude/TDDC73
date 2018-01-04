55424e7b720fc3cec64081e52a638fda87281d03
Samuel <samuel0808@gmail.com>
2018-01-04 17:52:07 +0100

    Comments added for Account registration, ready to hand in.

--- TDDC73_account_registration/app/src/main/java/example/samuel/tddc73_account_registration/AccountRegistration.java
+++ TDDC73_account_registration/app/src/main/java/example/samuel/tddc73_account_registration/AccountRegistration.java
@@ -19,57 +19,70 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 /**
+ *The component is made up of 9 main parts.
+ * The headline, it has the setter setHeadlineText.
  *
+ * These six parts a structured in a left and right column which are vertical linear layouts.
+ * They also all have the function set{fieldname}Required which takes a boolean and sets if the field
+ * is required to fill in to be able to register an account. If some field which is required is not
+ * filled in, a warning will appear by the field.
+ * Name field, it has the function removeName.
+ * Username, it has the function removeUsername.
+ * Password, it has the function removePassword.
+ * Email, it has the function removeEmail.
+ * Birthdate, it has the function removeDateOfBirth.
+ * Gender, it has the function removeGender.
+ *
+ * Terms of condition checkbox, it has the function removeTerms and setTermText.
+ * Register account button.
+ *
+ * There is a function setTextSize which sets the text size of all the field except the headline.
  */
 
 public class AccountRegistration extends LinearLayout {
 
-    //Params for left column texts
+    String headlineText;
+
+    //Variables for left column texts
     int textHeight = 90;
     int textSize = 11;
     LayoutParams textParams = new LayoutParams(LayoutParams.WRAP_CONTENT, textHeight);
     LinearLayout leftColumn;
 
     LinearLayout rightColumn;
-
+    //Name field variables
     TextView textViewName;
     EditText editTextName;
-
+    boolean nameRequired;
+    boolean nameEntered = false;
+    //Email variables
     TextView textViewEmail;
     EditText editTextEmail;
-
+    boolean emailRequired;
+    boolean emailEntered = false;
+    //Date of birth variables
     LinearLayout linearLayoutBirth;
-    TextView textViewDateOfbirth;
+    TextView textViewDateOfBirth;
     Spinner day;
     Spinner month;
     Spinner year;
     String[] days;
-
+    boolean dateOfBirthRequired;
+    //Password variables
     TextView textViewPassword;
     EditText editTextPassword;
-
-    TextView textViewUsername;
-    EditText editTextUsername;
-
-    TextView textViewGender;
-    Spinner genderDropdown;
-
-    boolean nameRequired;
-    boolean nameEntered = false;
-
-    boolean emailRequired;
-    boolean emailEntered = false;
-
-    boolean dateOfBirthRequired;
-
     boolean passwordRequired;
     boolean passwordEntered = false;
-
+    //Username variables
+    TextView textViewUsername;
+    EditText editTextUsername;
     boolean usernameRequired;
     boolean usernameEntered = false;
-
+    //Gender variables
+    TextView textViewGender;
+    Spinner genderDropdown;
     boolean genderRequired;
-
+    //Terms of condition variables
     LinearLayout linearLayoutTerms;
     String termText;
     CheckBox acceptTerms;
@@ -83,26 +96,31 @@ public class AccountRegistration extends LinearLayout {
         columnContainer.setOrientation(HORIZONTAL);
         columnContainer.setWeightSum(10);
         columnContainer.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-
+        //Left column
         leftColumn = new LinearLayout(context);
         leftColumn.setOrientation(LinearLayout.VERTICAL);
         LayoutParams leftColumnParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 2);
         leftColumn.setLayoutParams(leftColumnParams);
-
+        //Right column
         rightColumn = new LinearLayout(context);
         rightColumn.setOrientation(LinearLayout.VERTICAL);
         LayoutParams rightColumnParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 8);
         rightColumn.setLayoutParams(rightColumnParams);
-
+        //Headline
         TextView headline = new TextView(context);
-        headline.setText("Register a new account");
+        headlineText = "Register a new account";
+        headline.setText(headlineText);
         headline.setTextSize(20);
         headline.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
         this.addView(headline);
-
+        //Register button
         Button registerButton = new Button(context);
         registerButton.setText("Register account");
         registerButton.setOnClickListener(new OnClickListener() {
+            /**
+             * Checks if all the field that are required are set, if not field in, it displays error.
+             * @param view
+             */
             @Override
             public void onClick(View view) {
                 if (nameRequired && !nameEntered){
@@ -120,10 +138,10 @@ public class AccountRegistration extends LinearLayout {
                 if(dateOfBirthRequired){
                     if(year.getSelectedItemPosition() == 0 || month.getSelectedItemPosition() == 0
                             || day.getSelectedItemPosition() == 0){
-                        textViewDateOfbirth.setError("Choose a birthdate");
+                        textViewDateOfBirth.setError("Choose a birthdate");
                     }
                     else{
-                        textViewDateOfbirth.setError(null);
+                        textViewDateOfBirth.setError(null);
                     }
                 }
                 if(genderRequired){
@@ -142,6 +160,7 @@ public class AccountRegistration extends LinearLayout {
                 }
             }
         });
+        //Terms of condition
         linearLayoutTerms = new LinearLayout(context);
         linearLayoutTerms.setOrientation(HORIZONTAL);
         acceptTerms = new CheckBox(context);
@@ -152,6 +171,8 @@ public class AccountRegistration extends LinearLayout {
         linearLayoutTerms.addView(acceptTerms);
         linearLayoutTerms.addView(textViewTermText);
 
+        //Add the fields and set the initial values if the field is required or not.
+        //The set functions are public
         addName(context);
         setNameRequired(true);
         addUsername(context);
@@ -159,11 +180,11 @@ public class AccountRegistration extends LinearLayout {
         addPassword(context);
         setPasswordRequired(true);
         addEmail(context);
-        setEmailRequired(true);
+        setEmailRequired(false);
         addDateOfBirth(context);
-        setDateOfBirthRequired(true);
+        setDateOfBirthRequired(false);
         addGender(context);
-        setGenderRequired(true);
+        setGenderRequired(false);
 
         columnContainer.addView(leftColumn);
         columnContainer.addView(rightColumn);
@@ -219,11 +240,11 @@ public class AccountRegistration extends LinearLayout {
         rightColumn.addView(editTextEmail);
     }
     void addDateOfBirth(final Context context){
-        textViewDateOfbirth = new TextView(context);
-        textViewDateOfbirth.setTextSize(textSize);
-        textViewDateOfbirth.setLayoutParams(textParams);
-        textViewDateOfbirth.setGravity(Gravity.CENTER_VERTICAL);
-        leftColumn.addView(textViewDateOfbirth);
+        textViewDateOfBirth = new TextView(context);
+        textViewDateOfBirth.setTextSize(textSize);
+        textViewDateOfBirth.setLayoutParams(textParams);
+        textViewDateOfBirth.setGravity(Gravity.CENTER_VERTICAL);
+        leftColumn.addView(textViewDateOfBirth);
 
         linearLayoutBirth = new LinearLayout(context);
         linearLayoutBirth.setOrientation(HORIZONTAL);
@@ -245,6 +266,13 @@ public class AccountRegistration extends LinearLayout {
         final ArrayAdapter<String> dayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, days);
         day.setAdapter(dayAdapter);
         day.setOnTouchListener(new OnTouchListener() {
+            /**
+             * If the month is selected, display the right amount of days for that month in the
+             * day dropdown.
+             * @param view
+             * @param motionEvent
+             * @return
+             */
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
@@ -268,7 +296,7 @@ public class AccountRegistration extends LinearLayout {
         return yearList;
     }
     String[] getDayList(Integer month){
-        Calendar mycal = new GregorianCalendar(1990, month , 1);
+        Calendar mycal = new GregorianCalendar(2018, month , 1);
         Integer daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
         String[] dayList = new String[daysInMonth + 1];
         dayList[0] = "Day";
@@ -278,7 +306,6 @@ public class AccountRegistration extends LinearLayout {
         }
         return  dayList;
     }
-
 
     void addGender(Context context){
         textViewGender = new TextView(context);
@@ -380,10 +407,10 @@ public class AccountRegistration extends LinearLayout {
     public void setDateOfBirthRequired(boolean dateOfBirthRequired) {
         this.dateOfBirthRequired = dateOfBirthRequired;
         if(dateOfBirthRequired){
-            textViewDateOfbirth.setText("Birthdate*");
+            textViewDateOfBirth.setText("Birthdate*");
         }
         else{
-            textViewDateOfbirth.setText("Birthdate ");
+            textViewDateOfBirth.setText("Birthdate ");
         }
     }
 
@@ -434,7 +461,7 @@ public class AccountRegistration extends LinearLayout {
         rightColumn.removeView(editTextEmail);
     }
     public void removeDateOfBirth(){
-        leftColumn.removeView(textViewDateOfbirth);
+        leftColumn.removeView(textViewDateOfBirth);
         rightColumn.removeView(linearLayoutBirth);
     }
     public void removeGender(){
@@ -444,5 +471,7 @@ public class AccountRegistration extends LinearLayout {
     public void removeTerms(){
         this.removeView(linearLayoutTerms);
     }
-
+    public void setHeadlineText(String headlineText) {
+        this.headlineText = headlineText;
+    }
 }
